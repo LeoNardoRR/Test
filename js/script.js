cf4ea984e62fdaca4b35759ab5e3fc8ce4f2e1c6
LeoNardoRR <ribeiroleonardoti@gmail.com>
2023-06-05 16:22:53 -0300

    update

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// // Obtém todos os elementos <a> dentro do menu
+// const menuLinks = document.querySelectorAll('nav ul li a');
+
+// // Percorre os links do menu
+// menuLinks.forEach(link => {
+//   // Verifica se o link atual corresponde à página atual
+//   if (link.href === window.location.href) {
+//     // Adiciona a classe "active" ao link
+//     link.classList.add('active');
+//   }
+// });
